d4600046704644f04e71e2ca1bb37945ee48281a
wwren <rdding@student.unimelb.edu.au>
2025-06-08 12:22:54 +1000

    94 difference btw object.create(null) and F.proto = null

--- the_good_and_bad_bits_of_javascript/94_implement_object.create.js
+++ the_good_and_bad_bits_of_javascript/94_implement_object.create.js
@@ -29,3 +29,22 @@ function myObjectCreate2(proto) {
 
   return new Constructor();
 }
+
+/**
+ * How setting the prototype of a constructor function to null differs from using Object.create(null)
+ * https://stackoverflow.com/questions/18198178/null-prototype-object-prototype-and-object-create
+ * a function constructed with the new operator will always have an object prototype -> Object.prototype
+ * ECMA quote: if Type(proto) is not Object, set the [[Prototype]] internal property of obj to the standard built-in Object prototype object as described in 15.2.4.
+ *
+ * Object.create(o)
+ * ECMA quote: set the [[Prototype]] internal property of obj to O.
+ */
+function F() {}
+F.prototype = null;
+const f = new F();
+f.toString; // use Object.prototype.toString
+
+function F2() {}
+F2.prototype = Object.create(null);
+const f2 = new F2();
+f2.toString; // undefined
