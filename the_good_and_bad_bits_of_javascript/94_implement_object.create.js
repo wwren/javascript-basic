83afd96db76d244e9aa569489696fd05e464cd98
wwren <rdding@student.unimelb.edu.au>
2025-06-08 11:57:53 +1000

    94 object.create F.prototype

--- the_good_and_bad_bits_of_javascript/94_implement_object.create.js
+++ the_good_and_bad_bits_of_javascript/94_implement_object.create.js
@@ -1,6 +1,5 @@
 /**
- * @param {any} proto
- * @return {object}
+ * Solution using __proto__
  */
 function myObjectCreate(proto) {
   if (typeof proto !== "object" || !proto) {
@@ -11,4 +10,22 @@ function myObjectCreate(proto) {
   Object.setPrototypeOf(result, proto);
 
   return result;
+}
+
+/**
+ * Solution using F.prototype
+ * new objects can be created with a constructor function, like F()
+ * If F.prototype is an object, then the new operator uses it to set [[Prototype]] for the new object
+ */
+
+function myObjectCreate2(proto) {
+  if (typeof proto !== "object" || !proto) {
+    throw Error("error");
+  }
+
+  function Constructor() {}
+
+  Constructor.prototype = proto;
+
+  return new Constructor();
 }
